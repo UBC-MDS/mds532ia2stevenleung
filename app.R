2de8ad636e493581d04a59e25b46c98b0f5d9497
Steven Leung <klsleung@student.ubc.ca>
2022-03-11 01:09:07 -0800

    Write code.

--- app.R
+++ app.R
@@ -4,15 +4,47 @@ library(dashHtmlComponents)
 library(dashBootstrapComponents)
 library(ggplot2)
 library(plotly)
+library(tidyverse)
+library(dplyr)
 
 app <- Dash$new(external_stylesheets = dbcThemes$BOOTSTRAP)
 
-msleep2 <- readr::read_csv(here::here('data', 'msleep.csv'))
+traits_raw_df <- read_csv("data/breed_traits.csv")
+breed_rank_raw_df <- read_csv("data/breed_rank.csv")
+
+# Some data wrangling in preparation for the ranking trend plot
+
+traits_raw_df <- tibble::rowid_to_column(traits_raw_df, "BreedID")
+#head(traits_raw_df)
+breed_rank_df <- tibble::rowid_to_column(breed_rank_raw_df, "BreedID")
+#head(breed_rank_df)
+
+# Generate random score in lieu of input from the user.
+traits_df <- traits_raw_df %>%
+    mutate(score = stats::runif(nrow(traits_raw_df), 1, 100))
+#head(traits_df)
+
+top_5_raw_df <- traits_df %>%
+    slice_max(n=5, order_by = score)
+
+top_5_raw_df <- top_5_raw_df %>%
+    inner_join(breed_rank_df, by = c("BreedID")) %>%
+    mutate(Breed = Breed.x) %>%
+    select(!Breed.x)
+
+top_5_raw_df <- rename_with(top_5_raw_df, ~ gsub(" Rank", "", .x, fixed=TRUE))
+
+top_5_rank_df <- top_5_raw_df %>%
+    pivot_longer(20:27, names_to = "Rank_year", values_to = "Rank")
+
+traits_list_full <- top_5_rank_df %>%
+    select(-c("Breed" ,"Coat Type", "Coat Length")) %>%
+    colnames()
 
 app$layout(
     dbcContainer(
         list(
-            htmlH1('Dashr heroky deployment'),
+            htmlH1('Ranking of breeds'),
             dccGraph(id='plot-area'),
             htmlDiv(id='output-area'),
             htmlBr(),
@@ -20,8 +52,8 @@ app$layout(
             htmlBr(),
             dccDropdown(
                 id='col-select',
-                options = msleep2 %>% colnames %>% purrr::map(function(col) list(label = col, value = col)),
-                value='bodywt')
+                options = top_5_rank_df %>% colnames %>% purrr::map(function(col) list(label = col, value = col)),
+                value='Rank_year')
         )
     )
 )
@@ -30,13 +62,14 @@ app$callback(
     output('plot-area', 'figure'),
     list(input('col-select', 'value')),
     function(xcol) {
-        p <- ggplot(msleep2) +
-            aes(x = !!sym(xcol),
-                y = sleep_total,
-                color = vore,
-                text = name) +
-            geom_point() +
-            scale_x_log10() +
+        p <- top_5_rank_df %>%
+            ggplot(aes(x=!!sym(xcol),
+                       y=Rank,
+                       color=Breed)) +
+            geom_point() + 
+            scale_y_reverse() +
+            xlab("Rank year") +
+            ggtitle("Popularity ranking of breeds in recent years")
             ggthemes::scale_color_tableau()
         ggplotly(p) %>% layout(dragmode = 'select')
     }
@@ -53,3 +86,4 @@ app$callback(
 )
 
 app$run_server(host = '0.0.0.0')
+#app$run_server(debug = T)
