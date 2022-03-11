35e054a9952f80f9075c0818e50000b380fdb115
Steven Leung <klsleung@student.ubc.ca>
2022-03-11 10:11:30 -0800

    Add dashHtmlComponents to init.R.

--- init.R
+++ init.R
@@ -9,5 +9,5 @@ options(repos=r)
 # ======================================================================
 
 # packages go here
-install.packages(c('dash', 'readr', 'here', 'ggthemes', 'remotes', 'plotly', 'ggplot2', 'dplyr', 'tidyr'))
+install.packages(c('dash', 'readr', 'here', 'ggthemes', 'remotes', 'plotly', 'ggplot2', 'dplyr', 'tidyr', 'dashHtmlComponents'))
 remotes::install_github('facultyai/dash-bootstrap-components@r-release')
